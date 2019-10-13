3784fc5859f9e1327e31ce427027efb84078dcd9
Chuan Lin <clin43@buffalo.edu>
2019-10-13 12:32:14 -0400

    api check for contributions

--- send_sms.py
+++ send_sms.py
@@ -5,21 +5,38 @@ import json
 import config
 import datetime
 
-url ='https://github-contributions-api.now.sh/v1/clincl'
-#params = {}
-
-response = requests.get(url)
-year_range = 0
-for i in response.json()['years']:
-  year_range += 1
-print(year_range)
-x=datetime.datetime.now()
-print(x)
-print(response.url)
-con = response.json()['contributions']
-for c,i in enumerate(con):
-  if c < 4:
-    print(con[c]['date'],con[c]['count'])
+def get_message(user):
+  message = ""
+  url ='https://github-contributions-api.now.sh/v1/' + user
+  response = requests.get(url)
+  year_range = 0
+  # for i in response.json()['years']:
+  #   year_range += 1
+  # print(year_range)
+  # print(response.url)
+  contributions = response.json()['contributions']
+  date_today = str(datetime.date.today())
+  today_flag = False
+  streak = 0
+  # break if no streak or streak is broken
+  for c,i in enumerate(contributions):
+    if today_flag: # checks from yesterday and back
+      if contributions[c]['count'] != 0:
+        streak += 1
+      else:
+        #return streak
+        message += " You're current streak is " + str(streak) + " commits."
+        break
+    if  date_today == contributions[c]['date']: # look for today's date
+      today_flag = True
+      if contributions[c]['count'] != 0: # start streak count
+        streak += 1
+        message = "You commited github today!"
+      else: # remind user to commit and check for streak
+        message = "You didn't commit to github today!"
+        break
+  return message
+print(get_message("clincl"))
 #print(output)
 
 # # Your Account Sid and Auth Token from twilio.com/console
